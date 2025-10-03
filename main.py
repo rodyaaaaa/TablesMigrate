1fa613ce27c9187db9efcbc78ad71841e86be284
rodyaaaaa <rodion6568@gmail.com>
2025-10-03 15:20:43 +0300

    Enhance project setup: update .gitignore, add main functionality, and modify requirements

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+import pathlib
+import gspread
+import pandas
+
+from pygrister.api import GristApi
+
+
+SERVER = "http://192.168.2.89:8484"
+CONFIG = {
+    'GRIST_SELF_MANAGED': 'Y',
+    'GRIST_SELF_MANAGED_HOME': 'http://192.168.2.89:8484',
+    'GRIST_SELF_MANAGED_SINGLE_ORG': 'Y',
+    'GRIST_TEAM_SITE': 'docs',
+    'GRIST_API_KEY': '0295960cd81e2a23e3f4ac956a60ab086157c0d4',
+}
+
 def main():
-    pass
+    grist = GristApi()
+    grist.update_config(CONFIG)
+
+    pathlib.Path("tables").mkdir(exist_ok=True)
+
+    # Дивись https://docs.gspread.org/en/latest/oauth2.html - Enable API Access for a Project
+    gc = gspread.service_account(filename='service_google.json')
+    sh = gc.open_by_key("1hxCFLSM2Lj8PoGZJOf2pKmGCujoRKEPwS_Hd6LedMWg")
+    wks = sh.get_worksheet(0)
+
+    dataframe = pandas.DataFrame(wks.get_all_records())
+    print(dataframe.head(5))
+    
+    print("\nLIST RECORDS OF GRIST STYLESHEET")
+    grist.update_config({'GRIST_DOC_ID': 'suvirsYetpLL7ygw71w44o'})
+    print(grist.list_records('T1'))
 
 
 if __name__ == "__main__":
