a9933da8795bae553fcf64b81c8ccc60f641d57b
rodyaaaaa <rodion6568@gmail.com>
2025-10-03 17:01:52 +0300

    Refactor configuration and improve comments in main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import pandas
 
 from pygrister.api import GristApi
 
-
-SERVER = "http://192.168.2.89:8484"
+# Конфігурація для підключення до гріст
 CONFIG = {
     'GRIST_SELF_MANAGED': 'Y',
     'GRIST_SELF_MANAGED_HOME': 'http://192.168.2.89:8484',
     'GRIST_SELF_MANAGED_SINGLE_ORG': 'Y',
     'GRIST_TEAM_SITE': 'docs',
     'GRIST_API_KEY': '0295960cd81e2a23e3f4ac956a60ab086157c0d4',
+    'GRIST_WORKSPACE_ID': '0'
 }
 
 def main():
@@ -20,6 +20,7 @@ def main():
 
     pathlib.Path("tables").mkdir(exist_ok=True)
 
+    # Підключення до гугл документів
     # Дивись https://docs.gspread.org/en/latest/oauth2.html - Enable API Access for a Project
     gc = gspread.service_account(filename='service_google.json')
     sh = gc.open_by_key("1hxCFLSM2Lj8PoGZJOf2pKmGCujoRKEPwS_Hd6LedMWg")
@@ -28,9 +29,13 @@ def main():
     dataframe = pandas.DataFrame(wks.get_all_records())
     print(dataframe.head(5))
     
-    print("\nLIST RECORDS OF GRIST STYLESHEET")
+    # Отримуємо файл від Grist
     grist.update_config({'GRIST_DOC_ID': 'suvirsYetpLL7ygw71w44o'})
     print(grist.list_records('T1'))
+
+    # не працює поки що
+    id_doc = grist.add_doc("TEST")
+    print(id_doc)
 
 
 if __name__ == "__main__":
